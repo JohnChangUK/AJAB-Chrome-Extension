2a97e6771e0a4ad4224d6d826c60a6067010ef2d
Ashwini <ashie.oh@gmail.com>
2017-04-11 16:46:35 +0100

    Add test dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -10,14 +10,20 @@ module.exports = {
       filename: 'dist/bundle.js',
       sourceMapFilename: 'dist/bundle.map'
   },
+  externals: {
+    'cheerio': 'window',
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true,
+  },
   devtool: '#source-map',
   module: {
       loaders: [
           {
+            test: /\.jsx?$/,
             loader: 'babel-loader',
             exclude: /(node_modules)/,
             query: {
-              presets: ['react', 'es2015']
+              presets: ['react', 'es2015', 'stage-0']
             }
           }
       ]
