893ceabfbad86ecedc39c72282b20b440fdfb762
John Chang <johnchang9094@gmail.com>
2017-04-12 17:01:06 +0100

    Implementing Feature Tests

---
+++ test/feature.spec.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactTestUtils from 'react-addons-test-utils';
+import Widget from '../src/components/containers/Widget';
+
+it("render", () => {
+  
+})
